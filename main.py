d6fd747fa6cb39a075b038f85e118f7754ad86db
Matias Borba <matiasborba89@gmail.com>
2024-08-09 15:47:11 -0300

    test env variables

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify
 import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+    return jsonify({"Choo Choo": f"Welcome to your Flask app 🚅{os.getenv('TEST')}"})
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
