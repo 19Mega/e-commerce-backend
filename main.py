3c2d045ad97bf87b01fa6a68524d2cdf2cc3439e
Matias Borba <matiasborba89@gmail.com>
2024-08-15 14:44:58 -0300

    update

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from flask import Flask, jsonify
 import os
+import pymysql
+
+from flask import Flask, jsonify
 from dotenv import load_dotenv, find_dotenv
 from flask_sqlalchemy import SQLAlchemy
-import pymysql
+
+from datetime import timedelta 
+from flask_jwt_extended import JWTManager
 
 load_dotenv(find_dotenv())
 
@@ -11,19 +15,18 @@ pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
 
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=1)
+jwt = JWTManager(app)
+
 db_user = os.getenv('MYSQLUSER')
 db_password = os.getenv('MYSQLPASSWORD')
 db_host = os.getenv('MYSQLHOST')
 db_port = os.getenv('MYSQLPORT')
 db_name = os.getenv('MYSQLDATABASE')
 
-# Cambia la URI de la base de datos para usar PyMySQL
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///default.db' # To run it localy
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": f"Welcome to your Flask app 🚅{os.getenv('TEST', 'No test value')}"})
 
 db = SQLAlchemy(app)
 
@@ -34,5 +37,3 @@ with app.app_context():
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
-
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('MYSQL_URL', 'sqlite:///default.db')
--- routes.py
+++ routes.py
@@ -1,8 +1,18 @@
+import json
+
 from main import app, db
 from models import User 
+from flask import request, jsonify
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import create_access_token, get_csrf_token, jwt_required, get_jwt_identity
 
+bcrypt = Bcrypt()
 
 # -----
+
+@app.route('/')
+def index():
+    return jsonify({"Choo Choo": f"Welcome to your Flask app"})
 
 @app.route('/create_table')
 def create_table():
@@ -23,4 +33,82 @@ def delete_table():
     return "Tabla eliminada!"
 
 # -----
+
+# -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+# User : CREATE, READ, UPDATE, DELETE
+
+@app.route('/signup', methods=['POST'])
+def signup_user():
+    body = json.loads(request.data)
+
+    # user exist?
+    user = User.query.filter_by(email=body["email"]).first()
+    if user is None:  
+        # hashing the password
+        hashed_password = bcrypt.generate_password_hash(body["password"]).decode('utf-8')
+
+        new_user = User(
+            name = body["name"],
+            email = body["email"],
+            password = hashed_password,
+            is_active = body["is_active"],
+            is_admin = True,
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"msg": "User created."}), 200
+    
+    return jsonify({"msg": "User already exists."}), 400
+
+@app.route('/login', methods=['POST'])  
+def login_user():
+    body = json.loads(request.data)
+    email = body["email"]
+    password = body["password"]
+    
+    # user exist?
+    user = User.query.filter_by(email=email).first()
+    if not user or not bcrypt.check_password_hash(user.password, password): #check hashed password
+        return jsonify({'msg': 'Invalid username/password'}), 401
+
+    access_token = create_access_token(identity= user.id)
+    
+    favorites = Favorite.query.filter(Favorite.user_id == user.id).all()      
+    user_favorites = [favorite.serialize()['product_id'] for favorite in favorites]
+        
+    cart = Cart.query.filter(Cart.user_id == user.id).all()
+    user_cart = [{"id": cart.product_id, "quantity": cart.quantity} for cart in cart]
+    print(user_cart)
+    print(access_token)
+
+    if user.is_admin:
+        return jsonify(access_token= access_token, 
+                user_id= user.id,
+                user_name = user.name,
+                user_email = user.email,
+                user_favorites = user_favorites,
+                user_cart = user_cart,
+                user_admin = user.is_admin 
+                ), 200
+
+    return jsonify(access_token= access_token, 
+                    user_id= user.id,
+                    user_name = user.name,
+                    user_email = user.email,
+                    user_favorites = user_favorites,
+                    user_cart = user_cart   
+                    ), 200
+
+@app.route('/user/<int:user_id>/verify', methods=['GET'])
+@jwt_required()
+def veify_admin_token(user_id): 
+    # verify user
+    current_user_id = get_jwt_identity()
+    if current_user_id == user_id:
+        user = User.query.filter_by(id=user_id).first()
+        if user.is_admin: 
+            return jsonify(user_admin = user.is_admin), 200
+    
+    return jsonify({"msg": "Unauthorized."}), 401
+   
 
